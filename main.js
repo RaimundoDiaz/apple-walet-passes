4176bb84b01580d959e12c35608ece0bea7928f6
RaimundoDiaz <rediazparada@gmail.com>
2025-05-27 13:42:30 -0400

    Update passProperties in main.js with new program and organization names, and change logo and stamp image URIs for the loyalty pass

--- main.js
+++ main.js
@@ -11,17 +11,17 @@ const PassTypeEnum = {
 async function main() {
   // Properties for the loyalty pass
   const passProperties = {
-    programName: "My program name",
-    organizationName: "My organization",
-    logoUri:
-      "https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?t=st=1734533689~exp=1734537289~hmac=40f57aec2d4cb95a9fc0bd8e8f5e3bd279ac78ea24ff326d4a8e69d241e81937&w=1380",
-      qrCodeLink: "https://www.example.com/",
-      accountId: "SQ-12345A",
-    fullName: "Gon Freecss",
-    authenticationToken: "1234567890123456",
-    stampImageUri:
-      "https://images.unsplash.com/photo-1447933601403-0c6688de566e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&h=336"
-  };
+		programName: "Hunter License",
+		organizationName: "Hunter Association",
+		logoUri:
+			"https://static.wikia.nocookie.net/hunterxhunter/images/0/05/Hunter_Association_logo.png/revision/latest?cb=20180405075402",
+		qrCodeLink: "https://static1.srcdn.com/wordpress/wp-content/uploads/2022/07/Hunter-X-Hunter-Gon-Freecs-Piece-Sign.jpg?q=70&fit=contain&w=1200&h=628&dpr=1",
+		accountId: "SQ-12345A",
+		fullName: "Gon Freecss",
+		authenticationToken: "1234567890123456",
+		stampImageUri:
+			"https://images8.alphacoders.com/124/1243824.png"
+	};
 
   // Create the pass
   const pass = new StampPass();
